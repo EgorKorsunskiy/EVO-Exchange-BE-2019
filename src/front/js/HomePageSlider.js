2ded97665de8b070a7b8ca5bb917cd418b13873c
OleinikovArtem <avebyntar@gmail.com>
2020-02-02 16:26:53 +0200

    Home page slider complete

--- src/front/js/HomePageSlider.js
+++ src/front/js/HomePageSlider.js
@@ -1,35 +1,41 @@
-const line = document.querySelector('.Home-page-slider__line');
-const elements = document.querySelectorAll('.Home-page-slider__element');
-const sliderWedtch =  document.querySelector('.Home-page-slider').offsetWidth;
-let widthArray = [0];
-let lineWidth = 0;
-let offset = 0;
-let step = 0;
+const HSline = document.querySelector('.Home-page-slider__line');
+const HSelements = document.querySelectorAll('.Home-page-slider__inner');
+const HSsliderWidth =  document.querySelector('.Home-page-slider').offsetWidth;
+let HSwidthArray = [0];
+let HSlineWidth = 0;
+let HSoffset = 0;
+let HSstep = 0;
+let HSbalance = 0
 
-for (let i = 0; i < elements.length; i++) {
-    widthArray.push([elements[i].offsetWidth]);
-    lineWidth += elements[i].offsetWidth;
+for (let i = 0; i < HSelements.length; i++) {
+    HSwidthArray.push([HSelements[i].offsetWidth]);
+    HSlineWidth += HSelements[i].offsetWidth  ;
 }
-line.style.width  = lineWidth + 'px'
-
-console.log(line.style.left)
+HSline.style.width  = HSlineWidth + 'px'
 
 
+document.querySelector('.Home-page-slider').onclick = HomeSliderMove;
 
-document.querySelector('.Home-page-slider').onclick = function () {
-    offset = offset+widthArray[step];
-    line.style.left = offset+'px';
-    step++
-    console.log(line.style.left)
+function HomeSliderMove() {
+    HSbalance = HSlineWidth - HSsliderWidth - (+HSoffset + Number(HSwidthArray[HSstep]))
+
+    if(HSbalance >= 0 ) {
+        HSoffset = HSoffset + Number(HSwidthArray[HSstep]);
+        let offsets = Number(HSoffset)
+        HSline.style.left = Number(- offsets)+'px' ;
+        
+    }else {
+        HSline.style.left = -(HSlineWidth - HSsliderWidth) + 'px'
+        HSoffset  = 0;
+        HSstep = -1; 
+    }
+
+    if (HSstep + 1  === HSelements.length) {
+        HSstep = 0;
+        HSoffset = 0;
+    }else {
+        HSstep++
+    }
+   
 }
-
-// line.addEventListener('click' , slederMove )
-
-
-// function slederMove(event) {
-//     event.preventDefault();
-//     offset = offset+widthArray[step];
-//     line.style.left = offset+'px';
-//     step++
-//     console.log('ok')
-// }
+HomeSliderMove()
